ca336d136a2aa94c4798650f44e220280835abbf
ElMagdo <magdoe19@gmail.com>
2021-04-08 20:28:12 +0100

    Added code to print the remaining pattern.

--- JavaJavaJava/Task1/Patterns.java
+++ JavaJavaJava/Task1/Patterns.java
@@ -6,6 +6,8 @@ public class Patterns {
     public static void main(String[] args) {
         System.out.println("*****\n****\n***\n**\n*");
         System.out.println();
+        System.out.println("*****\n*   *\n*   *\n*   *\n*****");
+        System.out.println();
         System.out.println("*****\n* * *\n * * \n* * *\n*****");
     }
 }
