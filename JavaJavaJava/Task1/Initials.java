d8b4bc57ac985cd9934e78623d1b712f5d6def9f
ElMagdo <magdoe19@gmail.com>
2021-04-08 20:48:26 +0100

    Finished printing initials for first letter.

--- JavaJavaJava/Task1/Initials.java
+++ JavaJavaJava/Task1/Initials.java
@@ -1,5 +1,14 @@
 package JavaJavaJava.Task1;
 
 public class Initials {
-    
+    public static void main(String[] args) {
+        System.out.println("******");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*****");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("******");
+    }
 }
