147a7f581faa9de3fb3b937fa935c58b8dd0b884
ElMagdo <magdoe19@gmail.com>
2021-04-10 20:27:00 +0100

    Added the intro() method.

--- JavaJavaJava/Task3/TwelveDays.java
+++ JavaJavaJava/Task3/TwelveDays.java
@@ -1,5 +1,7 @@
 package JavaJavaJava.Task3;
 
 public class TwelveDays {
-    
+    public void intro(String day) {
+        System.out.println("On the " + day + "day of Christmas my true love gave to me,");
+    }
 }
