9ba51ec1456392a6b18e1d825172ba2225661889
ElMagdo <magdoe19@gmail.com>
2021-04-10 20:29:07 +0100

    Added day1() method.

--- JavaJavaJava/Task3/TwelveDays.java
+++ JavaJavaJava/Task3/TwelveDays.java
@@ -4,4 +4,8 @@ public class TwelveDays {
     public void intro(String day) {
         System.out.println("On the " + day + "day of Christmas my true love gave to me,");
     }
+
+    public void day1() {
+        System.out.println("a patridge in a pear tree");
+    }
 }
