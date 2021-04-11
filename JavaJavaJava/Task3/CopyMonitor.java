758efb00dc269e125f85a5e8f7119a4fcdeaff87
ElMagdo <magdoe19@gmail.com>
2021-04-11 15:09:33 +0100

    Added method to report based on machine's state.

--- JavaJavaJava/Task3/CopyMonitor.java
+++ JavaJavaJava/Task3/CopyMonitor.java
@@ -1,6 +1,15 @@
 package JavaJavaJava.Task3;
 
 public class CopyMonitor {
-    private boolean tonerLevel;
+    private boolean isLow;
     private boolean hasPrinted;
+
+    public String checkService(int tonerLevel, int pageCount) {
+        isLow = tonerLevel <= 10;
+        hasPrinted = pageCount >= 100000;
+
+        if (isLow) return "Service needed";
+        else if (hasPrinted) return "Service needed";
+        else return "Service not needed";
+    }
 }
