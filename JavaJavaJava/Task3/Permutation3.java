5e521e6a5b72baaae5901d2e358e624e26f8a3cd
ElMagdo <magdoe19@gmail.com>
2021-04-11 09:58:10 +0100

    Added print statements for the method.

--- JavaJavaJava/Task3/Permutation3.java
+++ JavaJavaJava/Task3/Permutation3.java
@@ -1,7 +1,13 @@
 package JavaJavaJava.Task3;
 
 public class Permutation3 {
-    public void permute(String s1, String s2, String s3) {
+    public void permute(String a, String b, String c) {
+        System.out.println(a + b + c);
+        System.out.println(a + c + b);
+        System.out.println(b + a + c);
+        System.out.println(b + c + a);
+        System.out.println(c + a + b);
+        System.out.println(c + b + a);
         return;
     }
 }
