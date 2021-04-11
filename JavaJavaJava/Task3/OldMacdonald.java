f42a733e3ef6dc0b4726fe20007a988aec54668d
ElMagdo <magdoe19@gmail.com>
2021-04-11 20:29:12 +0100

    Added hadAnX() method to class.

--- JavaJavaJava/Task3/OldMacdonald.java
+++ JavaJavaJava/Task3/OldMacdonald.java
@@ -4,4 +4,8 @@ public class OldMacdonald {
     public void eieio() {
         System.out.println("E I E I O");
     }
+
+    public void hadAnX(String s) {
+        System.out.println("Old MacDonald had a " + s);
+    }
 }
