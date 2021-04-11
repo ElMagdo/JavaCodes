11d40213dada002d0bc23428b583c47018e1ae06
ElMagdo <magdoe19@gmail.com>
2021-04-11 10:05:46 +0100

    Created clasas instance.

--- JavaJavaJava/Task3/Permutation3.java
+++ JavaJavaJava/Task3/Permutation3.java
@@ -16,6 +16,8 @@ public class Permutation3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        Permutation3 p = new Permutation3();
+
         String a, b, c;
 
         System.out.println("Enter first string: ");
