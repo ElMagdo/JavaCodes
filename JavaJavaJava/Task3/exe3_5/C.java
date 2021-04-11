18d7c83d67e8e9c76e3bbdd94a70ce691f5db5f0
ElMagdo <magdoe19@gmail.com>
2021-04-12 00:53:06 +0100

    Pasted class definition for class C.

--- JavaJavaJava/Task3/exe3_5/C.java
+++ JavaJavaJava/Task3/exe3_5/C.java
@@ -1,5 +1,15 @@
 package JavaJavaJava.Task3.exe3_5;
 
 public class C {
+    private int m;
+    private int n;
+
+    public C(int mIn , int nIn) {
+        m = mIn;
+        n = nIn;
+    }
     
+    public int m1() {
+        return m+n;
+    }
 }
