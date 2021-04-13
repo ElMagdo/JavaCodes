980cf18155754894836752b98ca40dd718f627a0
ElMagdo <magdoe19@gmail.com>
2021-04-13 21:14:22 +0100

    Added instance variables for various components.

--- JavaJavaJava/Task4/DisplayObject.java
+++ JavaJavaJava/Task4/DisplayObject.java
@@ -3,5 +3,7 @@ package JavaJavaJava.Task4;
 import javax.swing.*;
 
 public class DisplayObject extends JFrame {
-    
+    private JButton btn;
+    private JTextField fld;
+    private JLabel lbl;
 }
