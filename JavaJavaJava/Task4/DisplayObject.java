517c2fabd9462f1af74a46c7b34235c1b382bec2
ElMagdo <magdoe19@gmail.com>
2021-04-13 21:23:56 +0100

    Added constructor to the class.

--- JavaJavaJava/Task4/DisplayObject.java
+++ JavaJavaJava/Task4/DisplayObject.java
@@ -6,4 +6,10 @@ public class DisplayObject extends JFrame {
     private JButton btn;
     private JTextField fld;
     private JLabel lbl;
+
+    public DisplayObject(String t) {
+        setTitle(t);
+        pack();
+        setVisible(true);
+    }
 }
