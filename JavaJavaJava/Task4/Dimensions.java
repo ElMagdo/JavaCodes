1ff9382c19899aa01f89253179561ecbdd543f0b
ElMagdo <magdoe19@gmail.com>
2021-04-14 22:48:55 +0100

    Added class constructor.

--- JavaJavaJava/Task4/Dimensions.java
+++ JavaJavaJava/Task4/Dimensions.java
@@ -8,4 +8,11 @@ public class Dimensions extends JFrame {
     private JButton big;
     private JButton small;
     private JButton btn;
+
+    public Dimensions(String t) {
+        setTitle(t);
+        setSize(100, 100);
+        setLocation(200, 150);
+        setVisible(true);
+    }
 }
