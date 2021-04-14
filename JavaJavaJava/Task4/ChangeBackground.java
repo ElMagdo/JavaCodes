42816f9cba92e96f894ef0e5222841f0d8e14536
ElMagdo <magdoe19@gmail.com>
2021-04-14 23:35:21 +0100

    Created a class constructor.

--- JavaJavaJava/Task4/ChangeBackground.java
+++ JavaJavaJava/Task4/ChangeBackground.java
@@ -8,4 +8,11 @@ public class ChangeBackground extends JFrame {
     private JButton btn1;
     private JButton btn2;
     private JButton btn3;
+
+    public ChangeBackground(String t) {
+        setTitle(t);
+        setSize(300, 300);
+        setLocation(200, 150);
+        setVisible(true);
+    }
 }
