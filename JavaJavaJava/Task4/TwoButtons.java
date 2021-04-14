9db029b6091c573d54a4a67d3a2b89eba268ef35
ElMagdo <magdoe19@gmail.com>
2021-04-14 20:12:09 +0100

    Added the serialVersionUID.

--- JavaJavaJava/Task4/TwoButtons.java
+++ JavaJavaJava/Task4/TwoButtons.java
@@ -3,6 +3,7 @@ package JavaJavaJava.Task4;
 import javax.swing.*;
 
 public class TwoButtons extends JFrame {
+    private static final long serialVersionUID = 1L;
     private JButton btn1;
     private JButton btn2;
 }
