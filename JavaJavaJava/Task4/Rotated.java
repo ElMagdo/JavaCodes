7a8f10d19598d5d3dfb3aef20e1b326ad7430eea
ElMagdo <magdoe19@gmail.com>
2021-04-14 21:49:24 +0100

    added implements statement for ActionListener interface.

--- JavaJavaJava/Task4/Rotated.java
+++ JavaJavaJava/Task4/Rotated.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Rotated extends JFrame {
+public class Rotated extends JFrame implements ActionListener {
     private static final long serialVersionUID = 1L;
     private JButton first;
     private JButton second;
