7ecddab5b4c33338851e3798913d6d2564a3eaab
ElMagdo <magdoe19@gmail.com>
2021-04-14 09:50:43 +0100

    Created contructor to initialise the window.

--- JavaJavaJava/Task4/ToggleButton.java
+++ JavaJavaJava/Task4/ToggleButton.java
@@ -10,5 +10,12 @@ public class ToggleButton extends JFrame {
      */
     private static final long serialVersionUID = 1L;
     private JButton btn;
+
+    public ToggleButton(String t) {
+        setTitle(t);
+        setSize(200, 150);
+        setLocation(200, 150);
+        setVisible(true);
+    }
     
 }
