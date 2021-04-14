325ee718984082ceacc3af9a2017111fc63ebad4
ElMagdo <magdoe19@gmail.com>
2021-04-14 20:14:15 +0100

    Added a class Constructor.

--- JavaJavaJava/Task4/TwoButtons.java
+++ JavaJavaJava/Task4/TwoButtons.java
@@ -6,4 +6,11 @@ public class TwoButtons extends JFrame {
     private static final long serialVersionUID = 1L;
     private JButton btn1;
     private JButton btn2;
+
+    public TwoButtons(String t) {
+        setTitle(t);
+        setSize(200, 150);
+        setLocation(200, 150);
+        setVisible(true);
+    }
 }
