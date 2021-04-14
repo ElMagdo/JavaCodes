e0315f356dae8cd3a9901a0918f728e0b4b5154e
ElMagdo <magdoe19@gmail.com>
2021-04-14 23:32:57 +0100

    Added 3 JButton instance variables.

--- JavaJavaJava/Task4/ChangeBackground.java
+++ JavaJavaJava/Task4/ChangeBackground.java
@@ -5,5 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class ChangeBackground extends JFrame {
-    
+    private JButton btn1;
+    private JButton btn2;
+    private JButton btn3;
 }
