8d774099e166dde448d49d9e32edb88d4e16272a
ElMagdo <magdoe19@gmail.com>
2021-04-17 21:55:14 +0100

    Added serialVersionUID variable.

--- JavaJavaJava/Task5/TemperatureGUI.java
+++ JavaJavaJava/Task5/TemperatureGUI.java
@@ -5,5 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class TemperatureGUI extends JFrame implements ActionListener {
+
+    private static final long serialVersionUID = 1L;
     
 }
