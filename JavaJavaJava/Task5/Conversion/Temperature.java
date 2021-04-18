660f1d45b2775d3081e8cc5dcd9d4813185b22e5
ElMagdo <magdoe19@gmail.com>
2021-04-19 00:39:32 +0100

    Added first static method.

--- JavaJavaJava/Task5/Conversion/Temperature.java
+++ JavaJavaJava/Task5/Conversion/Temperature.java
@@ -2,4 +2,8 @@ package JavaJavaJava.Task5.Conversion;
 
 public class Temperature {
     public Temperature() {}
+
+    public static double celToFah(double cel) {
+        return (9.0 * cel/5.0 + 32.0) ;
+    }
 }
