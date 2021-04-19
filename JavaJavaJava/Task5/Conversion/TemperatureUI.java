0b9e7a52324eea6ad27fc4fdf17226747584cf6c
ElMagdo <magdoe19@gmail.com>
2021-04-19 11:26:28 +0100

    Added UI print statements.

--- JavaJavaJava/Task5/Conversion/TemperatureUI.java
+++ JavaJavaJava/Task5/Conversion/TemperatureUI.java
@@ -3,5 +3,9 @@ package JavaJavaJava.Task5.Conversion;
 import java.util.Scanner;
 
 public class TemperatureUI {
-    
+    public void run() {
+        System.out.println("Enter");
+        System.out.println("1. Convert from Celcius to Fahrenheit");
+        System.out.println("2. Convert from Fahrenheit to Celcius");
+    }
 }
