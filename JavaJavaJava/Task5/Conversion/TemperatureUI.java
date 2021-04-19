091715c8ff651809f0ce574ab70a7fef158ee602
ElMagdo <magdoe19@gmail.com>
2021-04-19 11:36:51 +0100

    Implemented the first choice of the user.

--- JavaJavaJava/Task5/Conversion/TemperatureUI.java
+++ JavaJavaJava/Task5/Conversion/TemperatureUI.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class TemperatureUI {
     public void run() {
         System.out.println("Enter");
-        System.out.println("1. Convert from Celcius to Fahrenheit");
-        System.out.println("2. Convert from Fahrenheit to Celcius");
+        System.out.println("1. Convert from Celsius to Fahrenheit");
+        System.out.println("2. Convert from Fahrenheit to Celsius");
 
         Scanner inp = new Scanner(System.in);
         int choice = inp.nextInt();
+
+        if (choice == 1) {
+            System.out.print("Enter temperature in Celcius: ");
+            double cel = inp.nextInt();
+            double fah = Temperature.celToFah(cel);
+            System.out.println(cel + "C = " + fah + "F");
+        }
     }
 }
