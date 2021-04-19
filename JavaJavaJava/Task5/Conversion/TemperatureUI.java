3b4e19ec230baabda91ed27d344282ba9d9629c3
ElMagdo <magdoe19@gmail.com>
2021-04-19 11:39:45 +0100

    Implemented the other choice of the user.

--- JavaJavaJava/Task5/Conversion/TemperatureUI.java
+++ JavaJavaJava/Task5/Conversion/TemperatureUI.java
@@ -16,6 +16,11 @@ public class TemperatureUI {
             double cel = inp.nextInt();
             double fah = Temperature.celToFah(cel);
             System.out.println(cel + "C = " + fah + "F");
+        } else if (choice == 2) {
+            System.out.print("Enter temperature in Fahrenheit: ");
+            double fah = inp.nextInt();
+            double cel = Temperature.celToFah(fah);
+            System.out.println(fah + "F = " + cel + "C");
         }
     }
 }
