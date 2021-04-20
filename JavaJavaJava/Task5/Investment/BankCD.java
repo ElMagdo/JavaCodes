f8898cc231b73c1a147ccc9435af3b8ada5d8d3a
ElMagdo <magdoe19@gmail.com>
2021-04-20 21:25:31 +0100

    Added class constructor.

--- JavaJavaJava/Task5/Investment/BankCD.java
+++ JavaJavaJava/Task5/Investment/BankCD.java
@@ -4,4 +4,10 @@ public class BankCD {
     private double principal;
     private double rate;
     private double years;
+
+    public BankCD(double p, double r, double y) {
+        principal = p;
+        rate = r;
+        years = y;
+    }
 }
