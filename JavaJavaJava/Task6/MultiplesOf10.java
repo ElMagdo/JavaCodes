d2fb9f7a9fd79e609551a74bc85d6668826d4a3c
ElMagdo <magdoe19@gmail.com>
2021-04-21 21:53:18 +0100

    Added Scanner object.

--- JavaJavaJava/Task6/MultiplesOf10.java
+++ JavaJavaJava/Task6/MultiplesOf10.java
@@ -1,7 +1,11 @@
 package JavaJavaJava.Task6;
 
+import java.util.Scanner;
+
 public class MultiplesOf10 {
     public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        
         System.out.println("Using for loop: ");
         System.out.println("Using while loop: ");
         System.out.println("Using do-while loop: ");
