2d2780ea9baf91473cd7629c524ddb23d713150e
ElMagdo <magdoe19@gmail.com>
2021-04-21 22:15:52 +0100

    Added Ui print statements.

--- JavaJavaJava/Task6/Sequence.java
+++ JavaJavaJava/Task6/Sequence.java
@@ -2,6 +2,8 @@ package JavaJavaJava.Task6;
 
 public class Sequence {
     public static void main(String[] args) {
-        
+        System.out.print("Using for loop: ");
+        System.out.print("Using while loop: ");
+        System.out.print("Using do-while loop: ");
     }
 }
