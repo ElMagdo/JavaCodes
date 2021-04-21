1600e2cc37cf950ac77793e856de026724bc2bef
ElMagdo <magdoe19@gmail.com>
2021-04-21 21:54:19 +0100

    Added variable to hold user's choice of loop.

--- JavaJavaJava/Task6/MultiplesOf10.java
+++ JavaJavaJava/Task6/MultiplesOf10.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class MultiplesOf10 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        
+
         System.out.println("Using for loop: ");
         System.out.println("Using while loop: ");
         System.out.println("Using do-while loop: ");
+
+        int loop = inp.nextInt();
     }
 }
