89fcef1ef7db74197a569fa08843ff03d7cf6660
ElMagdo <magdoe19@gmail.com>
2021-04-21 21:41:40 +0100

    Added main() and some print statements.

---
+++ JavaJavaJava/Task6/MultiplesOf10.java
@@ -0,0 +1,9 @@
+package JavaJavaJava.Task6;
+
+public class MultiplesOf10 {
+    public static void main(String[] args) {
+        System.out.println("Using for loop: ");
+        System.out.println("Using while loop: ");
+        System.out.println("Using do-while loop: ");
+    }
+}
