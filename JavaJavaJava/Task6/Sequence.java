966065371d99f3b6940ffb760e76da72aa0f3484
ElMagdo <magdoe19@gmail.com>
2021-04-21 22:32:29 +0100

    Added print statements to adjust output.

--- JavaJavaJava/Task6/Sequence.java
+++ JavaJavaJava/Task6/Sequence.java
@@ -4,20 +4,23 @@ public class Sequence {
     public static void main(String[] args) {
         System.out.print("Using for loop: ");
         for (int i = 45; i >= -45; i -= 9)
-            System.out.println(i);
+            System.out.print(i + " ");
+        System.out.println();
         
         System.out.print("Using while loop: ");
         int i = 45;
         while (i >= -45) {
-            System.out.println(i);
+            System.out.print(i + " ");
             i -= 9;
         }
+        System.out.println();
         
         System.out.print("Using do-while loop: ");
         i = 45;
         do {
-            System.out.println(i);
+            System.out.print(i + " ");
             i -= 9;
         } while (i >= -45);
+        System.out.println();
     }
 }
