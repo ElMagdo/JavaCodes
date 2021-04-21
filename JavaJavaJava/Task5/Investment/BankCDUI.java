b7f3700fd40493ac567c36472047c90f8f0a992b
ElMagdo <magdoe19@gmail.com>
2021-04-21 15:58:59 +0100

    Adjusted the display of the output.

--- JavaJavaJava/Task5/Investment/BankCDUI.java
+++ JavaJavaJava/Task5/Investment/BankCDUI.java
@@ -15,20 +15,20 @@ public class BankCDUI {
         System.out.println("************************ OUTPUT ********************");
         System.out.println("Compare daily and annual compounding for a Bank CD.");
 
-        System.out.print("Input CD initial principal, e.g. 1000.55 >");
+        System.out.print("Input CD initial principal, e.g. 1000.55 > ");
         p = inp.nextDouble();
 
-        System.out.print("Input CD interest rate, e.g. 6.5 >");
+        System.out.print("Input CD interest rate, e.g. 6.5 > ");
         r = inp.nextDouble();
 
-        System.out.print("Input the number of years to maturity, e.g., 10.5 >");
+        System.out.print("Input the number of years to maturity, e.g., 10.5 > ");
         n = inp.nextDouble();
 
         inp.close();
 
         BankCD bank = new BankCD(p, r, n);
         
-        System.out.println("For Principal = " + dollars.format(p) + " Rate= " + percent.format(r) + " Years= " + n);
+        System.out.println("For Principal = " + dollars.format(p) + " Rate = " + r + " Years = " + n);
         System.out.println("The maturity value compounded yearly is: " + dollars.format(bank.yearlyCD()));
         System.out.println("The maturity value compounded daily is: " + dollars.format(bank.dailyCD()));
         System.out.println("************************ OUTPUT ********************");
