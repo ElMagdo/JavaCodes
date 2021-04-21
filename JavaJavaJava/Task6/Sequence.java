db95573cc99016a955287356c99f95906268295e
ElMagdo <magdoe19@gmail.com>
2021-04-21 22:22:10 +0100

    Added code to print using for loop.

--- JavaJavaJava/Task6/Sequence.java
+++ JavaJavaJava/Task6/Sequence.java
@@ -3,6 +3,9 @@ package JavaJavaJava.Task6;
 public class Sequence {
     public static void main(String[] args) {
         System.out.print("Using for loop: ");
+        for (int i = 45; i >= -45; i -= 9)
+            System.out.println(i);
+        
         System.out.print("Using while loop: ");
         System.out.print("Using do-while loop: ");
     }
