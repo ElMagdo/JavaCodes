394998bf214e63f2ba09b4645f64af3d5dcdb750
ElMagdo <magdoe19@gmail.com>
2021-04-22 09:35:03 +0100

    Added the method for computation.

--- JavaJavaJava/Task6/EvenNumbers.java
+++ JavaJavaJava/Task6/EvenNumbers.java
@@ -1,6 +1,12 @@
 package JavaJavaJava.Task6;
 
 public class EvenNumbers {
+
+    public static void even(int n) {
+        for (int i = 1; i <= n; i++)
+            if (i%2 == 0)
+                System.out.println(i);
+    }
     public static void main(String[] args) {
         
     }
