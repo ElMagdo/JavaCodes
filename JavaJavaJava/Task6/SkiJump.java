4427ea0a207fb678c2ecc6d44d1522f1f8d463ff
ElMagdo <magdoe19@gmail.com>
2021-04-22 09:07:55 +0100

    Added UI print statements.

--- JavaJavaJava/Task6/SkiJump.java
+++ JavaJavaJava/Task6/SkiJump.java
@@ -2,6 +2,8 @@ package JavaJavaJava.Task6;
 
 public class SkiJump {
     public static void main(String[] args) {
-        
+        System.out.println("Using for loop:");
+        System.out.println("Using while loop:");
+        System.out.println("Using do-while loop");
     }
 }
