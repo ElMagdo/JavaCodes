4cbf94940993bfd2b878face8dfa15adb0d51cb7
ElMagdo <magdoe19@gmail.com>
2021-04-22 09:10:49 +0100

    Implemented printing using for loop.

--- JavaJavaJava/Task6/SkiJump.java
+++ JavaJavaJava/Task6/SkiJump.java
@@ -3,6 +3,12 @@ package JavaJavaJava.Task6;
 public class SkiJump {
     public static void main(String[] args) {
         System.out.println("Using for loop:");
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= i; j++)
+                System.out.print('*');
+            System.out.println();
+        }
+        
         System.out.println("Using while loop:");
         System.out.println("Using do-while loop");
     }
