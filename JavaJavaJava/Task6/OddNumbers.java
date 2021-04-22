1a6c4b10aacd07a9ab28c2621323587df7ede95c
ElMagdo <magdoe19@gmail.com>
2021-04-22 09:48:47 +0100

    Added the method for computation.

--- JavaJavaJava/Task6/OddNumbers.java
+++ JavaJavaJava/Task6/OddNumbers.java
@@ -1,5 +1,9 @@
 package JavaJavaJava.Task6;
 
 public class OddNumbers {
-    
+    public static void odd(int n) {
+        for (int i = 1; i <= n; i++)
+            if (i%2 == 1)
+                System.out.println(i);
+    }
 }
