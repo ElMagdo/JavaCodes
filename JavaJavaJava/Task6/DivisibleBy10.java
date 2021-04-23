e7e95afa4760198e087a4f28b1070d25e8b6f7ca
ElMagdo <magdoe19@gmail.com>
2021-04-23 07:44:30 +0100

    Added method for class.

--- JavaJavaJava/Task6/DivisibleBy10.java
+++ JavaJavaJava/Task6/DivisibleBy10.java
@@ -1,5 +1,10 @@
 package JavaJavaJava.Task6;
 
 public class DivisibleBy10 {
-    
+    public static void divisible(int n) {
+        for (int i = n; i > 0; i--) {
+            if (i%10 == 0)
+                System.out.println(i);
+        }
+    }
 }
