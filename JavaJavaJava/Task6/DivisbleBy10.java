b36b68efadd585d0fd03b0b9484c044525fee0a4
ElMagdo <magdoe19@gmail.com>
2021-04-27 08:00:32 +0100

    Added computation method.

--- JavaJavaJava/Task6/DivisbleBy10.java
+++ JavaJavaJava/Task6/DivisbleBy10.java
@@ -1,5 +1,10 @@
 package JavaJavaJava.Task6;
 
 public class DivisbleBy10 {
-    
+    public static void divisible(int n) {
+        for (int i = n; i > 0; i--) {
+            if (i % 10 == 0)
+                System.out.println(i);
+        }
+    }
 }
