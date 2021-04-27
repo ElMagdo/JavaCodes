99da1fb5dd0b47fde4673e1bdaf33b072c5bedc5
ElMagdo <magdoe19@gmail.com>
2021-04-27 08:03:45 +0100

    added code to get user input.

--- JavaJavaJava/Task6/DivisbleBy10.java
+++ JavaJavaJava/Task6/DivisbleBy10.java
@@ -12,5 +12,8 @@ public class DivisbleBy10 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int N = in.nextInt();
     }
 }
