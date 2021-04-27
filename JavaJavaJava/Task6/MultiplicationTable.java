42e4c25cdae9b500755870c58432f2fd6d80e5f9
ElMagdo <magdoe19@gmail.com>
2021-04-27 08:43:37 +0100

    Added first part of static method.

--- JavaJavaJava/Task6/MultiplicationTable.java
+++ JavaJavaJava/Task6/MultiplicationTable.java
@@ -3,4 +3,9 @@ public class MultiplicationTable {
         for (int i = 1; i <= n; i++)
             System.out.print(i * n + " ");
     }
+
+    public static void table(int l, int w) {
+        for (int i = 1; i <= w; i++)
+            System.out.print("\t" + i);
+    }
 }
