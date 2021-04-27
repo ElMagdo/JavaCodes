39cd60f0a6b0ca5d6031a2b3d8786d9854ed51ab
ElMagdo <magdoe19@gmail.com>
2021-04-27 08:02:22 +0100

    Added Scanner object.

--- JavaJavaJava/Task6/DivisbleBy10.java
+++ JavaJavaJava/Task6/DivisbleBy10.java
@@ -1,5 +1,7 @@
 package JavaJavaJava.Task6;
 
+import java.util.Scanner;
+
 public class DivisbleBy10 {
     public static void divisible(int n) {
         for (int i = n; i > 0; i--) {
@@ -9,6 +11,6 @@ public class DivisbleBy10 {
     }
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
     }
 }
