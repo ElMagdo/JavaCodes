2278a81608f8b623880dadbc3cf20f685b57e246
ElMagdo <magdoe19@gmail.com>
2021-04-27 11:44:51 +0100

    Added print statements to adjust output.

--- JavaJavaJava/Task6/MultiplicationTable.java
+++ JavaJavaJava/Task6/MultiplicationTable.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class MultiplicationTable {
     public static void uptoSquare(int n) {
         for (int i = 1; i <= n; i++)
-            System.out.print(i * n + " ");
+            System.out.print(i * n + "   ");
     }
 
     public static void table(int l) {
+        System.out.print("    ");
         for (int i = 1; i <= l; i++)
-            System.out.print("   " + i);
+            System.out.print(i + "   ");
 
         System.out.println();
 
         for (int i = 1; i <= l; i++) {
+            System.out.print(i + "   ");
             uptoSquare(i);
             System.out.println();
         }
