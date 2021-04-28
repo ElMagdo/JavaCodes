6e0f56c1b6ddb09c0ed727efb378aa778712a8cc
ElMagdo <magdoe19@gmail.com>
2021-04-28 20:31:38 +0100

    Added Scanner object.

--- JavaJavaJava/Task6/LargestSmallest.java
+++ JavaJavaJava/Task6/LargestSmallest.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class LargestSmallest {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        in.close();
     }
 }
