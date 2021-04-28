ae2bcdf3a3cacbbf12214bb6c3c36d8f0e634d67
ElMagdo <magdoe19@gmail.com>
2021-04-28 20:06:15 +0100

    Updated the computation method.

--- JavaJavaJava/Task6/AsteriskBox.java
+++ JavaJavaJava/Task6/AsteriskBox.java
@@ -2,10 +2,24 @@ import java.util.Scanner;
 
 public class AsteriskBox {
     public static void box(int r, int c) {
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= c; j++)
-                System.out.print('*');
+        for (int i = 1; i <= c; i++) {
+            System.out.print(" * ");
+        }
+
+        System.out.println();
+
+        for (int j = 1; j <= c-1; j++) {
+            System.out.print(" * ");
+
+            for (int i = 1; i <= r-2; i++)
+                System.out.print("   ");
+
+            System.out.print(" * ");
             System.out.println();
+        }
+
+        for (int i = 1; i <= c; i++) {
+            System.out.print(" * ");
         }
     }
 
@@ -20,5 +34,7 @@ public class AsteriskBox {
         in.close();
 
         box(row, col);
+
+        System.out.println();
     }
 }
