b8532bc9517da64da081339c69ce0989ce65117d
ElMagdo <magdoe19@gmail.com>
2021-04-28 20:34:46 +0100

    Added UI statements.

--- JavaJavaJava/Task6/LargestSmallest.java
+++ JavaJavaJava/Task6/LargestSmallest.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class LargestSmallest {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        in.close();
-    }
-}
+
+        System.out.println("Keep entering a positive number:");
+        System.out.println("Smallest number: \nLargest number: ");
