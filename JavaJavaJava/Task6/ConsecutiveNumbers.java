f258ab0378a6700490ecbc5b1bb4db0124986d82
ElMagdo <magdoe19@gmail.com>
2021-04-28 20:17:20 +0100

    Added loop functionality.

--- JavaJavaJava/Task6/ConsecutiveNumbers.java
+++ JavaJavaJava/Task6/ConsecutiveNumbers.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class ConsecutiveNumbers {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        System.out.println("Just keep entering a smaller number");
+
+        int i = in.nextInt();
+        while(in.hasNext()) {
+            int j = in.nextInt();
+
+            if (j > i) break;
+        }
+        
         in.close();
     }
 }
