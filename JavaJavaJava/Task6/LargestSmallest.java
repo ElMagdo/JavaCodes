40f7398c10a71e0f9c866606aaac2a3604514d3a
ElMagdo <magdoe19@gmail.com>
2021-04-28 21:15:51 +0100

    Added loop functionality.

--- JavaJavaJava/Task6/LargestSmallest.java
+++ JavaJavaJava/Task6/LargestSmallest.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class LargestSmallest {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int i, j, l, s;
+        int i, l, s;
 
         System.out.println("Keep entering a positive number:");
+        i = in.nextInt();
+        l = s = i;
 
-        System.out.println("The Smallest number: \nLargest number: ");
+        while(i > 0) {
+            l = (l > i) ? l : i;
+            s = (s < i) ? s : i;
+            i = in.nextInt();
+        }
+
+        System.out.println("Smallest number: " + s + "\nLargest number: " + l);
         in.close();
     }
 }
