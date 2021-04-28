b275986d3615d1153dd83b36e083b014ade058aa
ElMagdo <magdoe19@gmail.com>
2021-04-28 20:53:24 +0100

    Added variables to hold user input and output data.

--- JavaJavaJava/Task6/LargestSmallest.java
+++ JavaJavaJava/Task6/LargestSmallest.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class LargestSmallest {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int i, j, l, s;
 
         System.out.println("Keep entering a positive number:");
-        System.out.println("Smallest number: \nLargest number: ");
+
+        System.out.println("The Smallest number: \nLargest number: ");
+        in.close();
+    }
+}
