03c5f4c10ebcc8185e807f8d8c417a14de21bef7
ElMagdo <magdoe19@gmail.com>
2021-04-30 10:37:13 +0100

    Changed parameter type in the printRow() method.

--- JavaJavaJava/Task6/PrintPatterns.java
+++ JavaJavaJava/Task6/PrintPatterns.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class PrintPatterns {
-    public static void printRow(int s, int e, char c) {
+    public static void printRow(int s, int e, String c) {
         for (int i = s; i <= e; i++)
             System.out.print(c);
-        //System.out.println();
     }
 
     public static void firstShape(int n) {
         for (int i = 1; i <= n; i++) {
-            printRow(0, i, ' ');
-            printRow(0, n-i, '*');
+            printRow(0, i, "  ");
+            printRow(0, n-i, "* ");
             System.out.println();
         }
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter the size of the shapes: ");
         int sz = in.nextInt();
-        
+
         in.close();
     }
 }
