043289f527bc240edda5f1f7d783447a450701ad
ElMagdo <magdoe19@gmail.com>
2021-04-30 10:30:31 +0100

    Added UI statements.

--- JavaJavaJava/Task6/PrintPatterns.java
+++ JavaJavaJava/Task6/PrintPatterns.java
@@ -16,6 +16,10 @@ public class PrintPatterns {
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the size of the shapes: ");
+        int sz = in.nextInt();
+        
         in.close();
     }
 }
