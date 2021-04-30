af1d74e6fb2322b1b287eea2dc612d6db5dfe371
ElMagdo <magdoe19@gmail.com>
2021-04-30 10:16:24 +0100

    Added a static method for printing rows.

--- JavaJavaJava/Task6/PrintPatterns.java
+++ JavaJavaJava/Task6/PrintPatterns.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class PrintPatterns {
+    public static void printRow(int s, int e, char c) {
+        for (int i = s; i <= e; i++)
+            System.out.print(c);
+        System.out.println();
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         in.close();
