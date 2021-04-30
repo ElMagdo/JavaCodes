ef8ad433ad72bf64459abe07ae9dcc15bdf6a145
ElMagdo <magdoe19@gmail.com>
2021-04-30 10:27:54 +0100

    Added method to print first shape.

--- JavaJavaJava/Task6/PrintPatterns.java
+++ JavaJavaJava/Task6/PrintPatterns.java
@@ -4,7 +4,15 @@ public class PrintPatterns {
     public static void printRow(int s, int e, char c) {
         for (int i = s; i <= e; i++)
             System.out.print(c);
-        System.out.println();
+        //System.out.println();
+    }
+
+    public static void firstShape(int n) {
+        for (int i = 1; i <= n; i++) {
+            printRow(0, i, ' ');
+            printRow(0, n-i, '*');
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
