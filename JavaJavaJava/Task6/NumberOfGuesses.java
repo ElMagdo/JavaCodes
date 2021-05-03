a02fae44c5ea6e9b5e568134f5fa04ef9bc3b459
ElMagdo <magdoe19@gmail.com>
2021-05-03 21:16:15 +0100

    Added class contsructor.

--- JavaJavaJava/Task6/NumberOfGuesses.java
+++ JavaJavaJava/Task6/NumberOfGuesses.java
@@ -3,5 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class NumberOfGuesses extends JFrame {
-    
+    public NumberOfGuesses(String t) {
+        setTitle(t);
+        setSize(300, 300);
+        setLocation(200, 150);
+        setVisible(true);
+    }
 }
