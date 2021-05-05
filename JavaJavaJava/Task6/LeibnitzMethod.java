c9e1a5f52b3672d997b2fa08b3845788dc2b156c
ElMagdo <magdoe19@gmail.com>
2021-05-05 10:07:51 +0100

    Added static computation method.

--- JavaJavaJava/Task6/LeibnitzMethod.java
+++ JavaJavaJava/Task6/LeibnitzMethod.java
@@ -1,3 +1,18 @@
 public class LeibnitzMethod {
-    
+    public static int iterations(double s, double e) {
+        int it = 0, i = 1;
+        double sum = 0.0;
+
+        do {
+            sum = (it%2 == 0) ? sum + (1.0/i) : sum - (1.0/i);
+            i += 2;
+            it++;
+
+            if ((4*sum >= s) && (4*sum >= e)) {
+                System.out.println("sum = " + 4*sum);
+                return it;
+            }
+        }
+        while(true);
+    }
 }
