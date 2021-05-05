18760398d170d24037f064263f3c792735ab7db6
ElMagdo <magdoe19@gmail.com>
2021-05-05 10:46:42 +0100

    Added static computation method.

--- JavaJavaJava/Task6/NewtonSqrt.java
+++ JavaJavaJava/Task6/NewtonSqrt.java
@@ -1,3 +1,12 @@
 public class NewtonSqrt {
-    
+    public static int squareRoot(double g, double r) {
+        int i = 0;
+
+        while (g != Math.sqrt(r)) {
+            g = ((r / g) + g) / 2;
+            i++;
+        }
+
+        return i;
+    }
 }
