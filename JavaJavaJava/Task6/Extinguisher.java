83a2310027f9eabc5fd790b9ab64acb4b599d0cd
ElMagdo <magdoe19@gmail.com>
2021-05-06 08:20:58 +0100

    Added class Constructor.

--- JavaJavaJava/Task6/Extinguisher.java
+++ JavaJavaJava/Task6/Extinguisher.java
@@ -3,5 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Extinguisher extends JFrame implements ActionListener {
-    
+    public Extinguisher(String t) {
+        setTitle(t);
+        setLocation(150, 200);
+        setSize(200, 300);
+        setVisible(true);
+    }
 }
