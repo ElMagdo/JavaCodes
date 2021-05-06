686c2f13345628b5d6cdef5f8dff5129d6e2fd34
ElMagdo <magdoe19@gmail.com>
2021-05-06 08:04:29 +0100

    Added class constructor.

--- JavaJavaJava/Task6/NPrimes.java
+++ JavaJavaJava/Task6/NPrimes.java
@@ -3,5 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class NPrimes extends JFrame implements ActionListener {
-    
+    public NPrimes(String t) {
+        setTitle(t);
+        setSize(200, 300);
+        setLocation(150, 200);
+        setVisible(true);
+    }
 }
