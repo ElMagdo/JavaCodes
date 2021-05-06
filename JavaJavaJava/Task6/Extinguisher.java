4681312d4ac1ed0b636935f27140f40b76aa421c
ElMagdo <magdoe19@gmail.com>
2021-05-06 08:33:02 +0100

    Added method for the UI display.

--- JavaJavaJava/Task6/Extinguisher.java
+++ JavaJavaJava/Task6/Extinguisher.java
@@ -5,8 +5,13 @@ import java.awt.event.*;
 public class Extinguisher extends JFrame implements ActionListener {
     public Extinguisher(String t) {
         setTitle(t);
+        buildUI();
         setLocation(150, 200);
         setSize(200, 300);
         setVisible(true);
+    }
+
+    public void buildUI() {
+        int i;
     }
 }
