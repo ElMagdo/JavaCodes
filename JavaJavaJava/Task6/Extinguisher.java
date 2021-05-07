aae8dde00c3406dadb3e167a3128113e63998238
ElMagdo <magdoe19@gmail.com>
2021-05-07 13:41:12 +0100

    Added class instance variables.

--- JavaJavaJava/Task6/Extinguisher.java
+++ JavaJavaJava/Task6/Extinguisher.java
@@ -3,6 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Extinguisher extends JFrame implements ActionListener {
+    private JLabel lx, ly;
+    private JTextField tx, ty;
+    private JButton btn;
+    private JTextArea disp;
+    
     public Extinguisher(String t) {
         setTitle(t);
         buildUI();
