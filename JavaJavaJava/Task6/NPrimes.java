40b7aa8a4e1e1e9eaaeb0cf0acba0a0faac40fe6
ElMagdo <magdoe19@gmail.com>
2021-05-08 20:10:37 +0100

    Added UI method.

--- JavaJavaJava/Task6/NPrimes.java
+++ JavaJavaJava/Task6/NPrimes.java
@@ -4,9 +4,14 @@ import java.awt.event.*;
 
 public class NPrimes extends JFrame implements ActionListener {
     public NPrimes(String t) {
+        buildUI();
         setTitle(t);
         setSize(200, 300);
         setLocation(150, 200);
         setVisible(true);
+    }
+
+    public void buildUI() {
+        int i;
     }
 }
