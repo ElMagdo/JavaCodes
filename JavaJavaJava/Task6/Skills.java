6e7b263ad689e87be3fc2830f669434a1c4ac09c
ElMagdo <magdoe19@gmail.com>
2021-05-10 10:21:52 +0100

    Added method to display title.

--- JavaJavaJava/Task6/Skills.java
+++ JavaJavaJava/Task6/Skills.java
@@ -1,3 +1,9 @@
 public class Skills {
-    
+    public void displayTitle() {
+        System.out.println("*******************************");
+        System.out.println("*                             *");
+        System.out.println("*    MULTIPLICATION TESTER    *");
+        System.out.println("*                             *");
+        System.out.println("*******************************");
+    }
 }
