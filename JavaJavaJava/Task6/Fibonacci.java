32148dbfdec946fb5b8c64a62c15db82973a5a43
ElMagdo <magdoe19@gmail.com>
2021-05-11 22:44:54 +0100

    Updated the static method.

--- JavaJavaJava/Task6/Fibonacci.java
+++ JavaJavaJava/Task6/Fibonacci.java
@@ -4,12 +4,20 @@ public class Fibonacci {
     public static void fibonacci(int n) {
         int n0 = 0, n1 = 1, fn = 0;
 
-        for (int i = 1; i <= n; i++) {
-            fn = n0 + n1;
-            System.out.print(fn + " ");
-            n0 = n1;
-            n1 = fn;
+        if (n == 1) System.out.print(n-1);
+        if (n == 2) System.out.print("0 " + (n-1));
+
+        if (n > 2) {
+            System.out.print("0 1 ");
+            for (int i = 1; i <= n-2; i++) {
+                fn = n0 + n1;
+                System.out.print(fn + " ");
+                n0 = n1;
+                n1 = fn;
+            }
         }
+
+        System.out.println("\n");
     }
 
     public static void main(String[] args) {
