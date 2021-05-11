9c3c9084aabf3bcb32e4aa6f31169417573b98b6
ElMagdo <magdoe19@gmail.com>
2021-05-11 22:27:10 +0100

    Added static computation method.

--- JavaJavaJava/Task6/Fibonacci.java
+++ JavaJavaJava/Task6/Fibonacci.java
@@ -1,2 +1,12 @@
 public class Fibonacci {
+    public static void fibonacci(int n) {
+        int n0 = 0, n1 = 1, fn = 0;
+
+        for (int i = 1; i <= n; i++) {
+            fn = n0 + n1;
+            System.out.print(fn + " ");
+            n0 = n1;
+            n1 = fn;
+        }
+    }
 }
