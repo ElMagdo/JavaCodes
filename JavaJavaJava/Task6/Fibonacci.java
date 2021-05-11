a0815f73a2cb9dc785f5d985879ead2c516aa210
ElMagdo <magdoe19@gmail.com>
2021-05-11 22:30:19 +0100

    Added UI statements.

--- JavaJavaJava/Task6/Fibonacci.java
+++ JavaJavaJava/Task6/Fibonacci.java
@@ -14,6 +14,9 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a positive number to generate Fibonacci series: ");
+        int n = in.nextInt();
         in.close();
     }
 }
