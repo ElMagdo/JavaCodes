cb834a94f7a94ea7080cf61c440bb32d9cc4d78c
ElMagdo <magdoe19@gmail.com>
2021-05-13 22:53:46 +0100

    Added UI print statements.

--- JavaJavaJava/Task6/HorizontalBars.java
+++ JavaJavaJava/Task6/HorizontalBars.java
@@ -17,6 +17,14 @@ public class HorizontalBars {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int a, b, c, d;
+
+        System.out.print("Enter the 4 values of the Bar chart: ");
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+        d = in.nextInt();
+        
         in.close();
     }
 }
