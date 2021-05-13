9a1c0dc9e2f757f543b2962fc4decee142d19988
ElMagdo <magdoe19@gmail.com>
2021-05-14 00:03:15 +0100

    Added next part of computation method.

--- JavaJavaJava/Task6/VerticalBars.java
+++ JavaJavaJava/Task6/VerticalBars.java
@@ -2,5 +2,15 @@ public class VerticalBars {
     public static void displayBar(int a, int b, int c, int d) {
         int mx = Math.max(Math.max(a, b), Math.max(c, d));
         String ch = "** ", sp = "   ";
+
+        for (int i = mx; i > 0; i--) {
+            if (i <= a) System.out.print(ch);
+            else System.out.print(sp);
+
+            if (i <= b) System.out.print(ch);
+            else System.out.print(sp);
+
+            System.out.println();
+        }
     }
 }
