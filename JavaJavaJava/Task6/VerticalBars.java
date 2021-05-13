a9bd75a9037a56ab0693123197f764d8bc612451
ElMagdo <magdoe19@gmail.com>
2021-05-14 00:01:16 +0100

    Added a computation method.

---
+++ JavaJavaJava/Task6/VerticalBars.java
@@ -0,0 +1,6 @@
+public class VerticalBars {
+    public static void displayBar(int a, int b, int c, int d) {
+        int mx = Math.max(Math.max(a, b), Math.max(c, d));
+        String ch = "** ", sp = "   ";
+    }
+}
