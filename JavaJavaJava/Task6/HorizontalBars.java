58033e4a0448610e58b29d5f351dc52f02e53ad4
ElMagdo <magdoe19@gmail.com>
2021-05-13 22:45:38 +0100

    Added method to print bars.

--- JavaJavaJava/Task6/HorizontalBars.java
+++ JavaJavaJava/Task6/HorizontalBars.java
@@ -1,4 +1,11 @@
 public class HorizontalBars {
+    public static void printBar(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    
     public static void bargraph(int a, int b, int c, int d) {
         int i;
     }
