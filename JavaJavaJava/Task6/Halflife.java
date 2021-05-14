cd08c2ee9823a02dbe584e83b9a29b16f9e1d10b
ElMagdo <magdoe19@gmail.com>
2021-05-14 16:26:31 +0100

    Added Scanner object.

--- JavaJavaJava/Task6/Halflife.java
+++ JavaJavaJava/Task6/Halflife.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Halflife {
     public static int decay(double y) {
         int v = 100, j = 100, i = 0;
@@ -8,5 +10,10 @@ public class Halflife {
         }
 
         return i;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.close();
     }
 }
