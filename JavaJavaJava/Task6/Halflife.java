2855f866c6314f24c04b49c497a5cc4c8b483d35
ElMagdo <magdoe19@gmail.com>
2021-05-14 16:25:13 +0100

    Added static computation method.

--- JavaJavaJava/Task6/Halflife.java
+++ JavaJavaJava/Task6/Halflife.java
@@ -1,3 +1,12 @@
 public class Halflife {
-    
+    public static int decay(double y) {
+        int v = 100, j = 100, i = 0;
+
+        while (j > 0.02*v) {
+            i += y;
+            j *= 0.5;
+        }
+
+        return i;
+    }
 }
