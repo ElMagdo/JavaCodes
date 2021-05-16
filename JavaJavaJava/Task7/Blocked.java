6536f117902e4cf8de00c1ab45ac7c1a920e72d6
ElMagdo <magdoe19@gmail.com>
2021-05-16 17:57:21 +0100

    Added first part of static method.

--- JavaJavaJava/Task7/Blocked.java
+++ JavaJavaJava/Task7/Blocked.java
@@ -2,6 +2,11 @@ import java.util.StringTokenizer;
 
 public class Blocked {
     public static void block(int n, String str) {
-        
+        StringTokenizer st = new StringTokenizer(str);
+        String s = "";
+
+        while (st.hasMoreTokens()) {
+            s += st.nextToken();
+        }
     }
 }
