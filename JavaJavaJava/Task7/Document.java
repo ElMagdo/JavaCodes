e89fd7809f801347859f3190c89910449eaadaea
ElMagdo <magdoe19@gmail.com>
2021-05-17 23:39:44 +0100

    Added class and interface inheritance.

--- JavaJavaJava/Task7/Document.java
+++ JavaJavaJava/Task7/Document.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Document {
+public class Document extends JFrame implements ActionListener {
     private JTextArea doc;
     private JLabel hdx, numWords, numChars, sixMore;
 }
