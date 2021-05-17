3668db79139a8b00bad7351f868607c87f933d1e
ElMagdo <magdoe19@gmail.com>
2021-05-17 23:42:26 +0100

    Added UI method.

--- JavaJavaJava/Task7/Document.java
+++ JavaJavaJava/Task7/Document.java
@@ -7,9 +7,14 @@ public class Document extends JFrame implements ActionListener {
     private JLabel hdx, numWords, numChars, sixMore;
 
     public Document(String t) {
+        buildUI();
         setTitle(t);
         setSize(300, 300);
         setLocation(150, 200);
         setVisible(true);
+    }
+
+    public void buildUI() {
+        int i;
     }
 }
