f41fcd68665dd87d4ee83d81ef9ed43585a37907
ElMagdo <magdoe19@gmail.com>
2021-05-20 23:52:43 +0100

    Added class constructor.

--- JavaJavaJava/Task7/NumberToString.java
+++ JavaJavaJava/Task7/NumberToString.java
@@ -3,5 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class NumberToString extends JFrame implements ActionListener {
-    
+    public NumberToString(String t) {
+        setTitle(t);
+        setSize(300, 300);
+        setLocation(150, 200);
+        setVisible(true);
+    }
 }
