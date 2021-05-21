7d300b88ec64f0e450d005f42d50c9fbf5d74b5b
ElMagdo <magdoe19@gmail.com>
2021-05-21 23:39:04 +0100

    Added class constructor.

--- JavaJavaJava/Task7/Palindrome.java
+++ JavaJavaJava/Task7/Palindrome.java
@@ -3,5 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Palindrome extends JFrame implements ActionListener {
-    
+    public Palindrome(String t) {
+        setTitle(t);
+        setSize(300, 300);
+        setLocation(150, 200);
+        setVisible(true);
+    }
 }
