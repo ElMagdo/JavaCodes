01f54cbae305f9b91fb748ebad9508a79fe5e732
ElMagdo <magdoe19@gmail.com>
2021-05-21 23:43:53 +0100

    Added UI method.

--- JavaJavaJava/Task7/Palindrome.java
+++ JavaJavaJava/Task7/Palindrome.java
@@ -4,11 +4,14 @@ import java.awt.event.*;
 
 public class Palindrome extends JFrame implements ActionListener {
     public Palindrome(String t) {
+        biuldUI();
         setTitle(t);
         setSize(300, 300);
         setLocation(150, 200);
         setVisible(true);
     }
+
+    public void biuldUI() {}
 
     public static void main(String[] args) {
         new Palindrome("Check Palindromes");
