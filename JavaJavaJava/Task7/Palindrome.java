e02145ce7059721ad0f459845d87aef14864c3bf
ElMagdo <magdoe19@gmail.com>
2021-05-23 21:18:05 +0100

    Added JPanel object to hold components.

--- JavaJavaJava/Task7/Palindrome.java
+++ JavaJavaJava/Task7/Palindrome.java
@@ -7,17 +7,22 @@ public class Palindrome extends JFrame implements ActionListener {
     private JTextField txt;
 
     public Palindrome(String t) {
-        biuldUI();
+        buildUI();
         setTitle(t);
         setSize(300, 300);
         setLocation(150, 200);
         setVisible(true);
     }
 
-    public void biuldUI() {
+    public void buildUI() {
         hdx = new JLabel("Enter a word or a phrase:");
         txt = new JTextField(10);
         disp = new JLabel("Nothing yet!");
+
+        JPanel ct = new JPanel();
+        ct.add(hdx);
+        ct.add(txt);
+        ct.add(disp);
     }
 
     public static void main(String[] args) {
