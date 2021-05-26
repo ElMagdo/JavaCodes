531f55ee3dca837e388c3bbc9b871f28c82f36e6
ElMagdo <magdoe19@gmail.com>
2021-05-26 01:12:18 +0100

    Added UI statements.

--- JavaJavaJava/Task7/MailingLabel.java
+++ JavaJavaJava/Task7/MailingLabel.java
@@ -14,6 +14,10 @@ public class MailingLabel {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the string label: ");
+        String str = in.nextLine();
+        
         in.close();
     }
 }
