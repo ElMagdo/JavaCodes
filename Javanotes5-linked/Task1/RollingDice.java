cfda4c07ed08ff031ce0319ca4c9276060ba441c
ElMagdo <magdoe19@gmail.com>
2021-05-26 20:01:43 +0100

    Called the computation method in main().

--- Javanotes5-linked/Task1/RollingDice.java
+++ Javanotes5-linked/Task1/RollingDice.java
@@ -1,6 +1,9 @@
+package Task1;
+
 public class RollingDice {
     public static void roll(int i) {
-        int d1, d2;
+        int d1 = (int)(Math.random() * 6) + 1;
+        int d2;
 
         if (i == 1) {
             d1 = (int)(Math.random() * 6) + 1;
@@ -8,11 +11,13 @@ public class RollingDice {
         } else {
             d2 = (int)(Math.random() * 6) + 1;
             System.out.println("The second die comes up " + d2);
-            System.out.println("Your total roll is " + d1 + d2);
+            System.out.println("Your total roll is " + (d1 + d2));
         }
     }
 
     public static void main(String[] args) {
         System.out.println("+++++WELCOME++++++");
+        roll(1);
+        roll(2);
     }
 }
