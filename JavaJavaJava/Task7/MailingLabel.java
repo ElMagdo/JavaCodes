61b45d755a10df63de787bf763a23cace28069c3
ElMagdo <magdoe19@gmail.com>
2021-05-26 01:07:02 +0100

    Added static method to handle display of output.

--- JavaJavaJava/Task7/MailingLabel.java
+++ JavaJavaJava/Task7/MailingLabel.java
@@ -1,3 +1,13 @@
+import java.util.StringTokenizer;
+
 public class MailingLabel {
-    
+    public static void display(String str) {
+        StringTokenizer tk = new StringTokenizer(str, ":");
+
+        System.out.println("====================");
+        while (tk.hasMoreTokens()) {
+            System.out.println(tk.nextToken());
+        }
+        System.out.println("====================");
+    }
 }
