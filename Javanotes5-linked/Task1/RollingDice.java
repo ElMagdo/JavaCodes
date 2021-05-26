df2f13fad482ebf4bb9fbbb6989e48f2edd6afb4
ElMagdo <magdoe19@gmail.com>
2021-05-26 19:50:56 +0100

    Added computation function.

---
+++ Javanotes5-linked/Task1/RollingDice.java
@@ -0,0 +1,14 @@
+public class RollingDice {
+    public static void roll(int i) {
+        int d1, d2;
+
+        if (i == 1) {
+            d1 = (int)(Math.random() * 6) + 1;
+            System.out.println("The first die comes up " + d1);
+        } else {
+            d2 = (int)(Math.random() * 6) + 1;
+            System.out.println("The second die comes up " + d2);
+            System.out.println("Your total roll is " + d1 + d2);
+        }
+    }
+}
