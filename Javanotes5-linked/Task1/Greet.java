13c870fec793cf25921cf22ebbfbe010c52b1ba4
ElMagdo <magdoe19@gmail.com>
2021-05-27 20:17:45 +0100

    Added Scanner object.

--- Javanotes5-linked/Task1/Greet.java
+++ Javanotes5-linked/Task1/Greet.java
@@ -1,6 +1,14 @@
 package Task1;
+
+import java.util.Scanner;
+
 public class Greet {
     public static void greet(String name) {
         System.out.println("Hello, " + name + ", nice to meet you!");
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.close();
     }
 }
