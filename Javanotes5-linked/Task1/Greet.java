24d29f1b06d1d3b36accf9d5c7b8c9855aa1b852
ElMagdo <magdoe19@gmail.com>
2021-05-27 20:19:17 +0100

    Added UI statements.

--- Javanotes5-linked/Task1/Greet.java
+++ Javanotes5-linked/Task1/Greet.java
@@ -9,6 +9,10 @@ public class Greet {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        System.out.print("Please input your name: ");
+        String name = in.nextLine();
+        
         in.close();
     }
 }
