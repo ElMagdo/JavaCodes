b76cac3ffb386a62d0254cd6f21983ae92e2628a
ElMagdo <magdoe19@gmail.com>
2021-05-28 21:23:21 +0100

    Added computation method.

--- Javanotes5-linked/Task1/CountEggs.java
+++ Javanotes5-linked/Task1/CountEggs.java
@@ -1,5 +1,14 @@
 package Task1;
 
 public class CountEggs {
-    
+    public static void count(int n) {
+        int gross, dozen, extra;
+        gross = n / 144;
+        extra = n % 144;
+
+        dozen = extra / 12;
+        extra = extra % 12;
+
+        System.out.println("Your number of eggs is " + gross + " gross, " + dozen + " dozen, and " + extra);
+    }
 }
