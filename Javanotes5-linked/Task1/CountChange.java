020c99b37061cd47f48d7ab52beec40fcf008780
ElMagdo <magdoe19@gmail.com>
2021-05-28 20:52:34 +0100

    added Scanner object.

--- Javanotes5-linked/Task1/CountChange.java
+++ Javanotes5-linked/Task1/CountChange.java
@@ -1,5 +1,10 @@
 package Task1;
 
+import java.util.Scanner;
+
 public class CountChange {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.close();
+    }
 }
