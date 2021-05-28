e303f6222eb29f42f722002d5cf7f2c243e8b2e5
ElMagdo <magdoe19@gmail.com>
2021-05-28 20:58:43 +0100

    Added UI statements.

--- Javanotes5-linked/Task1/CountChange.java
+++ Javanotes5-linked/Task1/CountChange.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class CountChange {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        int quarters, dimes, nickels, pennies;
+
+        System.out.print("Enter number of Quarters: ");
+        quarters = in.nextInt();
+        System.out.print("Enter number of Dimes: ");
+        dimes = in.nextInt();
+        System.out.print("Enter number of Nickels: ");
+        nickels = in.nextInt();
+        System.out.print("Enter number of Pennies: ");
+        pennies = in.nextInt();
+
         in.close();
     }
 }
