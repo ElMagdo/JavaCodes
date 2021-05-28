32aee2d29e03224f94fdf47ca977b55b6dad733d
ElMagdo <magdoe19@gmail.com>
2021-05-28 21:26:55 +0100

    Added UI statements.

--- Javanotes5-linked/Task1/CountEggs.java
+++ Javanotes5-linked/Task1/CountEggs.java
@@ -1,5 +1,7 @@
 package Task1;
 
+import java.util.Scanner;
+
 public class CountEggs {
     public static void count(int n) {
         int gross, dozen, extra;
@@ -10,5 +12,13 @@ public class CountEggs {
         extra = extra % 12;
 
         System.out.println("Your number of eggs is " + gross + " gross, " + dozen + " dozen, and " + extra);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter number of eggs: ");
+        int eggs = in.nextInt();
+        in.close();
     }
 }
