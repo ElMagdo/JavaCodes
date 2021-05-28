487c26dabac289228cb5024842c5bdf52f9522e8
ElMagdo <magdoe19@gmail.com>
2021-05-28 21:13:32 +0100

    Re-arranged the computation method.

--- Javanotes5-linked/Task1/RollingDice.java
+++ Javanotes5-linked/Task1/RollingDice.java
@@ -1,23 +1,18 @@
 package Task1;
 
 public class RollingDice {
-    public static void roll(int i) {
-        int d1 = (int)(Math.random() * 6) + 1;
-        int d2;
-
-        if (i == 1) {
-            d1 = (int)(Math.random() * 6) + 1;
-            System.out.println("The first die comes up " + d1);
-        } else {
-            d2 = (int)(Math.random() * 6) + 1;
-            System.out.println("The second die comes up " + d2);
-            System.out.println("Your total roll is " + (d1 + d2));
-        }
+    public static void roll() {
+        int d = (int)(Math.random() * 6) + 1, sum = 0;
+        System.out.println("First die comes up " + d);
+        sum += d;
+        d = (int)(Math.random() * 6) + 1;
+        System.out.println("Second die comes up " + d);
+        sum += d;
+        System.out.println("Your total roll is " + sum);
     }
 
     public static void main(String[] args) {
         System.out.println("+++++WELCOME++++++");
-        roll(1);
-        roll(2);
+        roll();
     }
 }
