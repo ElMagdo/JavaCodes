6281197b90c95038ef18773d9ea6591949525d07
ElMagdo <magdoe19@gmail.com>
2021-05-29 19:49:27 +0100

    Added UI statements.

--- Javanotes5-linked/Task1/Exam1.java
+++ Javanotes5-linked/Task1/Exam1.java
@@ -1,7 +1,27 @@
 package Task1;
 
+import java.util.Scanner;
+
 public class Exam1 {
     public static double getAverage(int m1, int m2, int m3) {
         return (m1 + m2 + m3) / 3.0;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = in.nextLine();
+
+        System.out.print("Enter first term average: ");
+        int m1 = in.nextInt();
+
+        System.out.print("Enter second term average: ");
+        int m2 = in.nextInt();
+
+        System.out.print("Enter third term average: ");
+        int m3 = in.nextInt();
+
+        in.close();
     }
 }
