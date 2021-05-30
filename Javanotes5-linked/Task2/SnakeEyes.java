e1a2b5e0963cc46ba5cef3666e33434c7086e12c
ElMagdo <magdoe19@gmail.com>
2021-05-30 20:41:03 +0100

    Added computation method.

--- Javanotes5-linked/Task2/SnakeEyes.java
+++ Javanotes5-linked/Task2/SnakeEyes.java
@@ -1,5 +1,15 @@
 package Task2;
 
 public class SnakeEyes {
-    
+    public static void playSnakeEyes() {
+        int d1 = (int)(Math.random()*6) + 1;
+        int d2 = (int)(Math.random()*6) + 1;
+        int i = 0;
+
+        while (d1 != 1 && d2 != 1) {
+            i++;
+        }
+
+        System.out.println("It took " + i + " throws to get Snake Eyes");
+    }
 }
