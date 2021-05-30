ebecc6e9177058399a13b14c3ee0a04d8996b841
ElMagdo <magdoe19@gmail.com>
2021-05-30 22:25:40 +0100

    Added computation method.

--- Javanotes5-linked/Task2/Expressions.java
+++ Javanotes5-linked/Task2/Expressions.java
@@ -1,5 +1,17 @@
 package Task2;
 
 public class Expressions {
-    
+    public static void evaluate(double a, String op, double b) {
+        double ans = 0;
+        boolean chk = true;
+
+        if (op == "+") ans = a + b;
+        else if (op == "-") ans = a - b;
+        else if (op == "*") ans = a * b;
+        else if (op == "/") ans = a / b;
+        else chk = false;
+
+        if (chk) System.out.println(a + " " + op + "" + b + " = " + ans);
+        else System.out.println("INVALID operator '" + op + "'!!");
+    }
 }
