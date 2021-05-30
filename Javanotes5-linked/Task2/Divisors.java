e553ca0f88568c998744c1b3a153f9aa37c823ed
ElMagdo <magdoe19@gmail.com>
2021-05-30 21:36:10 +0100

    Added static sub computation method.

--- Javanotes5-linked/Task2/Divisors.java
+++ Javanotes5-linked/Task2/Divisors.java
@@ -1,5 +1,13 @@
 package Task2;
 
 public class Divisors {
-    
+    public static int getNumberOfDivisors(int n) {
+        int d = 0;
+        
+        for (int i = 1; i < n/2; i++) {
+            if (n%i == 0) d++;
+        }
+
+        return d;
+    }
 }
