1077de859c8e498ef9e1799f0875a7c69d214c41
ElMagdo <magdoe19@gmail.com>
2021-05-30 21:39:21 +0100

    Added major computation method.

--- Javanotes5-linked/Task2/Divisors.java
+++ Javanotes5-linked/Task2/Divisors.java
@@ -3,11 +3,21 @@ package Task2;
 public class Divisors {
     public static int getNumberOfDivisors(int n) {
         int d = 0;
-        
+
         for (int i = 1; i < n/2; i++) {
             if (n%i == 0) d++;
         }
 
         return d;
+    }
+
+    public static int getHighest(int l, int u) {
+        int mx = 0;
+
+        for (int i = l; i <= u; i++) {
+            mx = Math.max(mx, getNumberOfDivisors(i));
+        }
+
+        return mx;
     }
 }
