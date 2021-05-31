a2f70c469e44902ddf5b134c5b9595704639017c
ElMagdo <magdoe19@gmail.com>
2021-05-31 20:11:06 +0100

    Added Scanner object and UI statement.

--- Javanotes5-linked/Task2/Words.java
+++ Javanotes5-linked/Task2/Words.java
@@ -1,5 +1,7 @@
 package Task2;
 
+import java.util.Scanner;
+
 public class Words {
     public static void printWords(String str) {
         for (int i = 0; i < str.length(); i++) {
@@ -8,5 +10,13 @@ public class Words {
             if (ch == '\'') System.out.print(ch);
             if (ch == ' ') System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a line of text: ");
+        String str = in.nextLine();
+        in.close();
     }
 }
