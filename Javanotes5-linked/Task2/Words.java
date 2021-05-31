9d3e7b49ca86b90bd770f63f6d903021c8ef6f8c
ElMagdo <magdoe19@gmail.com>
2021-05-31 20:08:43 +0100

    Added computation method.

--- Javanotes5-linked/Task2/Words.java
+++ Javanotes5-linked/Task2/Words.java
@@ -1,5 +1,12 @@
 package Task2;
 
 public class Words {
-    
+    public static void printWords(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLetter(ch)) System.out.print(ch);
+            if (ch == '\'') System.out.print(ch);
+            if (ch == ' ') System.out.println();
+        }
+    }
 }
