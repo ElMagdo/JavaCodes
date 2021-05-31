a126c6b1d306f8d6313d4ab356424c9987ce1bad
ElMagdo <magdoe19@gmail.com>
2021-05-31 19:46:18 +0100

    Added computation method.

--- Javanotes5-linked/Task1/CountChange.java
+++ Javanotes5-linked/Task1/CountChange.java
@@ -3,6 +3,11 @@ package Task1;
 import java.util.Scanner;
 
 public class CountChange {
+
+    public static void convert(double q, double d, double n, double p) {
+        double dollars = (q * 0.25) + (d * 0.10) + (n * 0.05) + (p * 0.01);
+        System.out.println("Your change is: $" + dollars);
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
