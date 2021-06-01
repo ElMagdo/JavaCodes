21d778f9007c6951d450bf9930e9799fe06ea7ba
ElMagdo <magdoe19@gmail.com>
2021-06-01 21:32:25 +0100

    Added computation method.

--- Javanotes5-linked/Task3/Capitalize.java
+++ Javanotes5-linked/Task3/Capitalize.java
@@ -1,5 +1,23 @@
 package Task3;
 
 public class Capitalize {
-    
+    public static void printCapitalize(String str) {
+        int j = 0;
+
+        while (!Character.isLetter(str.charAt(j))) {
+            j++;
+            if (str.length() == j) break;
+        }
+
+        Character.toUpperCase(str.charAt(j));
+        j++;
+
+        for (int i = j; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i)) && str.charAt(i-1) == ' ') {
+                Character.toUpperCase(str.charAt(i));
+            }
+            
+            System.out.print(str.charAt(i));
+        }
+    }
 }
