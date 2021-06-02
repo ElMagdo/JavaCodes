2a9ad8d577f2cb08b6ad5271917e696b8bab67a4
ElMagdo <magdoe19@gmail.com>
2021-06-02 20:26:01 +0100

    Updated the computation methods.

--- Javanotes5-linked/Task3/Hexadecimal.java
+++ Javanotes5-linked/Task3/Hexadecimal.java
@@ -8,18 +8,26 @@ public class Hexadecimal {
     
         if (c >= '0' && c <= '9') hv = Integer.parseInt("" + c);
         else {
-            switch(Character.toUpperCase(c)) {
+            c = Character.toUpperCase(c);
+
+            switch(c) {
                 case 'A': hv = 10;
+                        break;
 
                 case 'B': hv = 11;
+                        break;
 
                 case 'C': hv = 12;
+                        break;
 
                 case 'D': hv = 13;
+                        break;
 
                 case 'E': hv = 14;
+                        break;
 
                 case 'F': hv = 15;
+                        break;
 
                 default: hv = -1;
             }
@@ -33,10 +41,9 @@ public class Hexadecimal {
         
         for (int i = 0; i < str.length(); i++) {
             char ch = Character.toUpperCase(str.charAt(i));
-            boolean isHexLetter = ch >= 'A' && ch <= 'F';
-            boolean isHexNumber = Character.isDigit(str.charAt(i));
+            boolean isHexLetter = (ch >= 'A' && ch <= 'F') || Character.isDigit(str.charAt(i));
 
-            if (!isHexLetter || !isHexNumber) {
+            if (!isHexLetter) {
                 chk = false;
                 break;
             }
@@ -58,13 +65,13 @@ public class Hexadecimal {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("+++++++++++++++ WELCOME TO HEX TO DEC CONVERSION APP+++++++++++++++\n");
+        System.out.println("\n+++++++++++++++ WELCOME TO HEX TO DEC CONVERSION APP+++++++++++++++\n");
         System.out.print("Enter a valid hexadecimal value: ");
         String hex = in.next();
         
         if (isValidHex(hex))
         else System.out.println("ERROR!! Invalid hex character found...");
-        
+
         in.close();
     }
 }
