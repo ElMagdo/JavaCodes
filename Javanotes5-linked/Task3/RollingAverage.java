e9af96311a6796c7c58441c99e0671230ff1205e
ElMagdo <magdoe19@gmail.com>
2021-06-04 20:15:03 +0100

    Added inner computation method.

--- Javanotes5-linked/Task3/RollingAverage.java
+++ Javanotes5-linked/Task3/RollingAverage.java
@@ -1,5 +1,17 @@
 package Task3;
 
 public class RollingAverage {
-    
+    public static int getRoll(int n) {
+        int d1 = (int)(Math.random() * 6) + 1;
+        int d2 = (int)(Math.random() * 6) + 1;
+        int i = 0;
+
+        while (d1 + d2 != n) {
+            i++;
+            d1 = (int)(Math.random() * 6) + 1;
+            d2 = (int)(Math.random() * 6) + 1;
+        }
+
+        return i;
+    }
 }
